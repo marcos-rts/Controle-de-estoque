9d21a635da0fc0274bf0f92957f0f485cdf4d986
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-07-16 22:27:36 -0300

    create header and footer

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php include '../src/views/templates/header.php'; ?>
+<main>
+    <h2>Home</h2>
+    <p>Bem-vindo à página inicial.</p>
+</main>
+<?php include '../src/views/templates/footer.php'; ?>

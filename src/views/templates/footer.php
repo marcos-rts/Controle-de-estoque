3a709b5f01a31a1be15d29c2fcdc6f9241ae40b7
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-07-18 21:35:57 -0300

    update Header and Footer

--- src/views/templates/footer.php
+++ src/views/templates/footer.php
@@ -1,5 +1,9 @@
-<footer>
-        <p>&copy; 2024 Meu Site. Todos os direitos reservados.</p>
-    </footer>
+<footer class="bg-dark text-white text-center py-3 fixed-bottom">
+    <div class="container">
+        <p class="mb-0">&copy; 2024 Seu Nome ou Empresa. Todos os direitos reservados.</p>
+        <p class="mb-0">Desenvolvido por <a href="https://github.com/seu-usuario" class="text-white">Seu Nome</a></p>
+    </div>
+</footer>
 </body>
+
 </html>

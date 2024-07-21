d038566a04be3c11a10c255aec0d922ac87ca93b
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-07-21 16:00:53 -0300

    update Header

--- src/views/templates/header.php
+++ src/views/templates/header.php
@@ -5,33 +5,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seu Título</title>
-    <link rel="stylesheet" href="../../../assets/css/bootstrap.css"> <!-- Adicione seu CSS aqui -->
-    <link rel="stylesheet" href="../../../assets/css/bootstrap.min.css"> <!-- Adicione seu CSS aqui -->
+    <!-- CSS bootstrap -->
+    <link rel="stylesheet" href="../../../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../../../assets/css/bootstrap.min.css">
+
+    <!-- CSS customizzado -->
+     <link rel="stylesheet" href="../../../assets/css/dashboard.css">
 
 </head>
 
 <body>
     <header>
-        <!-- <h1>Bem-vindo ao Meu Site</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">Sobre</a></li>
-                <li><a href="contact.php">Contato</a></li>
+        <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Controle de estoque</a>
+            <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+            <ul class="navbar-nav px-3">
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link" href="#">Sair</a>
+                </li>
             </ul>
-        </nav> -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(Página atual)</span></a>
-                    <a class="nav-item nav-link" href="#">Destaques</a>
-                    <a class="nav-item nav-link" href="#">Preços</a>
-                    <a class="nav-item nav-link disabled" href="#">Desativado</a>
-                </div>
-            </div>
         </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div class="sidebar-sticky">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">
+                                    <span data-feather="home"></span>
+                                    Home <span class="sr-only">(atual)</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="file"></span>
+                                    Itens
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../products/list.php">
+                                    <span data-feather="shopping-cart"></span>
+                                    Produtos
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="users"></span>
+                                    Clientes
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="bar-chart-2"></span>
+                                    Relatórios
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="layers"></span>
+                                    Integrações
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
     </header>

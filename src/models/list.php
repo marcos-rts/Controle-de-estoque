31ce55af7aa58d8ad34a112b18124442dadc628d
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-07-21 12:30:16 -0300

    list view creation

--- src/models/list.php
+++ src/models/list.php
@@ -1,12 +1,34 @@
 <?php
-
-include '../../config/database.php';
-$pdo = Banco::conectar();
-$sql = 'SELECT * FROM TB_PRODUTOS ORDER BY Id DESC';
-foreach ($pdo->query($sql) as $row) {
-    echo $row['Id'] . ' ' . $row['NomeProduto'] . ' ' . $row['Quantidade'] . '<br>';
-}
-
-
-
+// include '../../config/database.php';
+// $pdo = Banco::conectar();
+// $sql = 'SELECT * FROM TB_PRODUTOS ORDER BY Id DESC';
+// foreach ($pdo->query($sql) as $row) {
+//     echo $row['Id'] . ' ' . $row['NomeProduto'] . ' ' . $row['Quantidade'] . '<br>';
+// }
 ?>
+
+<table class="table">
+    <!-- Cabeçalho -->
+    <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Produto</th>
+            <th scope="col">Quantidade</th>
+        </tr>
+    </thead>
+    <!-- Corpo -->
+    <tbody>
+        <!-- Comando PHP com SQL para trazer a lista -->
+        <?php
+        include '../../../config/database.php';
+        $pdo = Banco::conectar();
+        $sql = 'SELECT * FROM TB_PRODUTOS ORDER BY Id DESC';
+        foreach ($pdo->query($sql) as $row) {
+            echo '<th scope="row">' . $row['Id'] . ' </th>';
+            echo '<td>' . $row['NomeProduto'] . ' </td>';
+            echo '<td>' . $row['Quantidade'] . ' </td>';
+        }
+        ?>
+        <!-- Fim dos comandos -->
+    </tbody>
+</table>
---
+++ src/views/products/list.php
@@ -0,0 +1,12 @@
+<?php include '../templates/header.php' ?>
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="../../../public/index.php">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Lista</li>
+  </ol>
+</nav>
+
+<main>
+    <?php include '../movements/list.php' ?>
+</main>
+<?php include '../templates/footer.php' ?>

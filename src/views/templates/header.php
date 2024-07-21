1e68dd455e7149f5b537b70478a2854f23a4bfd5
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-07-21 17:56:47 -0300

    changed header

--- src/views/templates/header.php
+++ src/views/templates/header.php
@@ -10,13 +10,13 @@
     <link rel="stylesheet" href="../../../assets/css/bootstrap.min.css">
 
     <!-- CSS customizzado -->
-     <link rel="stylesheet" href="../../../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../../../assets/css/dashboard.css">
 
 </head>
 
 <body>
     <header>
-        <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
             <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Controle de estoque</a>
             <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
             <ul class="navbar-nav px-3">
@@ -25,51 +25,4 @@
                 </li>
             </ul>
         </nav>
-
-        <div class="container-fluid">
-            <div class="row">
-                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                    <div class="sidebar-sticky">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">
-                                    <span data-feather="home"></span>
-                                    Home <span class="sr-only">(atual)</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <span data-feather="file"></span>
-                                    Itens
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../products/list.php">
-                                    <span data-feather="shopping-cart"></span>
-                                    Produtos
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <span data-feather="users"></span>
-                                    Clientes
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <span data-feather="bar-chart-2"></span>
-                                    Relatórios
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <span data-feather="layers"></span>
-                                    Integrações
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
     </header>

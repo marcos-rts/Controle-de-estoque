7a4200593dabae370bd99f5f286e7f8f62060de1
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-07-21 22:24:27 -0300

    footer modification at the end of the page

--- src/views/templates/footer.php
+++ src/views/templates/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-white text-center py-3 fixed-bottom">
+<footer class="bg-dark text-white text-center py-3">
     <div class="container">
         <p class="mb-0">&copy; 2024 Marcos Alexandre. Todos os direitos reservados.</p>
         <p class="mb-0">Desenvolvido por <a href="https://github.com/marcos-rts" class="text-white">Marcos Alexandre</a></p>
